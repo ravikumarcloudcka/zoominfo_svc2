1bb0eae6a8b6b95d9a06b6209b94fdf3b9d1c9c8
ravipavi143 <ravikumarmani93@gmail.com>
2022-10-22 21:44:24 +0530

    changes in response message

--- service2.js
+++ service2.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Successful response from Service 2 when calling Service 1');
+  res.send('Hello!!, This is successful response from Service 2 when calling Service 1');
 });
 
 app.listen(9090, () => console.log('Example app is listening on port 9090.'));
