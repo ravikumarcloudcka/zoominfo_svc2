7fa4955201780cb63f579a68a36d7df412aeacb9
ravikumar_repo <54144086+ravikumarcloudcka@users.noreply.github.com>
2023-11-08 13:34:32 +0530

    Update service2.js

--- service2.js
+++ service2.js
@@ -5,5 +5,5 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello!!, This is successful response from Service 2 when calling Service 1');
 });
-.
+
 app.listen(9090, () => console.log('Example app is listening on port 9090.'));
